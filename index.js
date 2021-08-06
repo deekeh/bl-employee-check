7912ee5c5929808f913791742edf05e2fc7afddb
DK <darshankhot@programmer.net>
2021-08-06 10:40:46 +0530

    added commenting

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 console.log("Welcome to Employee Wage Computation"); // uc1
 
+// uc2
 let empchk = Math.floor(Math.random() * 10) % 2;
-
 if (empchk) console.log("employee is present");
 else console.log("employee is absent");
 
+// uc3
 const fulltime = 8, parttime = 4;
 let hrs;
 let empCase = Math.floor(Math.random() * 10) % 2;
