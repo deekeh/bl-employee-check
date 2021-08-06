49d2feaae9d4f74098c537777b3d4e965ecf5a43
DK <darshankhot@programmer.net>
2021-08-06 10:23:12 +0530

    added uc2

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Welcome to Employee Wage Computation"); // uc1
+
+let empchk = Math.floor(Math.random() * 10) % 2;
+
+if (empchk) console.log("employee is present");
+else console.log("employee is absent");
