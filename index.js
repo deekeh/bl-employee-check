0d07bd4123d817375e671323851ba6a60ec3801f
DK <darshankhot@programmer.net>
2021-08-06 10:39:26 +0530

    added uc3

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ let empchk = Math.floor(Math.random() * 10) % 2;
 
 if (empchk) console.log("employee is present");
 else console.log("employee is absent");
+
+const fulltime = 8, parttime = 4;
+let hrs;
+let empCase = Math.floor(Math.random() * 10) % 2;
+
+switch (empCase) {
+  case 0:
+    hrs = parttime;
+    break;
+  case 1:
+    hrs = fulltime;
+    break;
+}
+
+console.log("Employee wage:", hrs*20);
