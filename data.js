49b2d13e81d07c312bfd67ed0358a7cab4c4d8c6
DK <darshankhot@programmer.net>
2021-08-07 21:22:10 +0530

    added uc6 getRestrictedWage() function to calculate the wage for a month with 20day and 160hr restrictions

--- data.js
+++ data.js
@@ -1,4 +1,7 @@
 module.exports = {
+  dailyWage: 20,
   fullTime: 8,
   halfTime: 4,
+  maxHours: 160,
+  maxDays: 20,
 }
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const {fullTime: fulltime, halfTime: parttime} = require('./data');
+const {
+  fullTime: fulltime,
+  halfTime: parttime,
+  maxHours,
+  maxDays,
+  dailyWage,
+} = require('./data');
 
 console.log("Welcome to Employee Wage Computation"); // uc1
 
@@ -24,12 +30,12 @@ switch (empCase) {
     break;
 }
 
-console.log("Employee wage:", hrs*20);
+console.log("Employee wage:", hrs * dailyWage);
 
 // uc4
 function getMonthlyWage() {
   let monthlyPay = 0;
-  const parttime = 4, fulltime = 8;
+  // const parttime = 4, fulltime = 8;
   let hrs;
   for (let day = 0; day<20; day++) {
     let empCase = Math.floor(Math.random() * 10) %3;
@@ -44,7 +50,7 @@ function getMonthlyWage() {
         hrs = 0;
         break;
     }
-    monthlyPay += hrs*20;
+    monthlyPay += hrs * dailyWage;
   }
   return monthlyPay;
 }
@@ -52,6 +58,43 @@ function getMonthlyWage() {
 // uc5
 console.log("Monthly pay:", getMonthlyWage());
 
+// uc6
 function getRestrictedWage() {
+  // setting initial values
+  let monthlyPay = 0, workingHours = 0, workingDays = 0, monthDays = 0;
 
+  // loop through 20 days of the month
+  while (workingHours <= maxHours && monthDays <= maxDays) {
+    monthDays += 1;
+    let currentWorking = 0;
+    const currentWorkingHours = Math.floor(Math.random() * 10) % 3;
+
+    // calculate daily pay as per attendance
+    switch (currentWorkingHours) {
+      case 0: // working parttime
+        currentWorking = parttime;
+        workingHours += parttime;
+        workingDays++;
+        break;
+      case 1: // working fulltime
+        currentWorking = fulltime;
+        workingHours += fulltime;
+        workingDays++;
+        break;
+      default: // on leave
+        break;
+    }
+    monthlyPay += currentWorking * dailyWage;
+
+  }
+  // return result
+  return {
+    monthlyPay,
+    workingHours,
+    workingDays
+  }
 }
+
+// const restrictedWage = getRestrictedWage();
+const restrictedWage = getRestrictedWage();
+console.log(`Monthly pay: ${restrictedWage.monthlyPay}\nWorking hours: ${restrictedWage.workingHours}/160\nWorking days: ${restrictedWage.workingDays}/20`);
