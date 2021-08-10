4a73e4f96954719a86b79c99a411543e54aed4f0
DK <darshankhot@programmer.net>
2021-08-10 14:59:09 +0530

    removed implementation of uc13

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -30,35 +30,3 @@ dk.eSalary = 17_000;
 
 console.log('Employee details:');
 console.log(`Name: ${dk.eName}, ID: ${dk.eId}, Salary: ${dk.eSalary}`);
-
-console.log('\n----- extended employee data -----\n');
-
-const NewEmployeePayrollData = class extends EmployeePayrollData {
-    // constructor(id=0, name='User', salary=0, gender='female', startDate='10 Aug 2021') {
-    constructor(...params) {
-        super(...params.slice(0,2));
-        this.gender = params[3];
-        this.startDate = params[4];
-    }
-
-    get eGender() {
-        return this.gender;
-    }
-    get eStartDate() {
-        return this.startDate;
-    }
-
-    set eGender(gender) {
-        this.gender = gender;
-    }
-    set eStartDate(startDate) {
-        this.startDate = startDate;
-    }
-};
-
-const niraj = new NewEmployeePayrollData(1, 'Niraj', 16_000, 'male', '20 Jul 2021');
-
-niraj.eStartDate = '21 Jul 2021';
-
-console.log('New Employee details:');
-console.log(`Name: ${niraj.eName}, ID: ${niraj.eId}, Salary: ${niraj.eSalary}, Gender: ${niraj.eGender}, Start date: ${niraj.eStartDate}`);
